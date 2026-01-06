8244cb920a96fa5a2aed67e6e189e61b56c9ba47
Jerin <jerinusrat001@gmail.com>
2026-01-06 15:52:42 +0600

    added host availability in the host-view page succesfully database is getting updated

---
+++ src/main/java/com/root/meetsync/repository/EventSlotRepository.java
@@ -0,0 +1,7 @@
+package com.root.meetsync.repository;
+
+import com.root.meetsync.entity.EventSlot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventSlotRepository extends JpaRepository<EventSlot, Long> {
+}
--- src/main/java/com/root/meetsync/repository/HostAvailabilityRepository.java
+++ src/main/java/com/root/meetsync/repository/HostAvailabilityRepository.java
@@ -1,9 +1,16 @@
 package com.root.meetsync.repository;
 
 import com.root.meetsync.entity.HostAvailability;
+import com.root.meetsync.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 public interface  HostAvailabilityRepository extends JpaRepository<HostAvailability, Long> {
+    List<HostAvailability> findByEventSlot_Event_Id(Long eventId);
+    @Transactional
+    void deleteByHostAndEventSlot_Event_Id(User user, Long eventId);
 
 }
