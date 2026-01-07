ebd2c1646bc63f20a108425a3c8d4ca980bd6702
Jerin <jerinusrat001@gmail.com>
2026-01-07 18:19:17 +0600

    successfully added confirm event along with reschedule functionalities, changed some ui regarding scroll bar,

---
+++ src/main/java/com/root/meetsync/entity/ConfirmedEvent.java
@@ -0,0 +1,55 @@
+package com.root.meetsync.entity;
+
+import jakarta.persistence.*;
+import org.aspectj.weaver.patterns.ConcreteCflowPointcut;
+
+import java.time.LocalDateTime;
+@Entity
+public class ConfirmedEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @OneToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @ManyToOne
+    @JoinColumn(name = "slot_id")
+    private EventSlot selectedSlots;
+
+    private LocalDateTime confirmedAt;
+    private String additionalNote;
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public EventSlot getSelectedSlots() {
+        return selectedSlots;
+    }
+
+    public void setSelectedSlots(EventSlot selectedSlots) {
+        this.selectedSlots = selectedSlots;
+    }
+
+    public LocalDateTime getConfirmedAt() {
+        return confirmedAt;
+    }
+
+    public void setConfirmedAt(LocalDateTime confirmedAt) {
+        this.confirmedAt = confirmedAt;
+    }
+    public String getAdditionalNote() { return additionalNote; }
+    public void setAdditionalNote(String additionalNote) { this.additionalNote = additionalNote; }
+}
